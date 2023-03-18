a922feb09e31fe3e951879f162c7c17376ef1b68
njpacudan <neltonpacudan486@gmail.com>
2023-03-19 03:22:01 +0800

    A3

--- createvehicle.py
+++ createvehicle.py
@@ -1,4 +1,4 @@
 import factory
 
-print ("Created a randomized vehicle")
 print ("Vehicle: "+factory.i+"\nNo. of wheels: "+str(factory.wheels)+"\nBrand: "+factory.z+"\nColour: "+factory.f+"\nTransmission: "+factory.d)
+        
